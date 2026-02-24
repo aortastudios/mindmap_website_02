512a446cddf4a49d7aa5454665156c4b06f9491a
nelson <chinwubanelson@gmail.com>
2026-02-24 09:58:11 +0100

    increased testimonial card number

--- src/utils/dummyTestimonial.ts
+++ src/utils/dummyTestimonial.ts
@@ -25,24 +25,91 @@ export const dummyTestimonial1 = [
     content:
       "Using MindMap made journaling feel natural for me. The prompts help me reflect without overthinking, and I’ve become more aware of my emotions.",
   },
+  {
+    id: 4,
+    src: "/images/hillary.png",
+    name: "Billy Hillary",
+    content:
+      "Navigating the search for a therapist with MindMap was effortless! My sessions have transformed my outlook and I now feel more equipped than ever to handle my emotions!",
+  },
+  {
+    id: 5,
+    src: "/images/lucas.png",
+    name: "Lucas Timothy",
+    content: "The daily reflections and reminders keep me grounded. ",
+  },
+  {
+    id: 6,
+    src: "/images/belinda.png",
+    name: "Belinda Walsh",
+    content:
+      "Using MindMap made journaling feel natural for me. The prompts help me reflect without overthinking, and I’ve become more aware of my emotions.",
+  },
+  {
+    id: 7,
+    src: "/images/hillary.png",
+    name: "Billy Hillary",
+    content:
+      "Navigating the search for a therapist with MindMap was effortless! My sessions have transformed my outlook and I now feel more equipped than ever to handle my emotions!",
+  },
+  {
+    id: 8,
+    src: "/images/lucas.png",
+    name: "Lucas Timothy",
+    content: "The daily reflections and reminders keep me grounded. ",
+  },
 ];
 
 export const dummyTestimonial2 = [
   {
-    id: 4,
+    id: 9,
     src: "/images/belinda.png",
     name: "Billy Hillary",
     content: "The daily reflections and reminders keep me grounded.",
   },
   {
-    id: 5,
+    id: 10,
     src: "/images/billy.png",
     name: "Lucas Timothy",
     content:
       "The daily reflections and reminders keep me grounded. It doesn’t feel pushy — just supportive when I need it. Always Recommend",
   },
   {
-    id: 6,
+    id: 11,
+    src: "/images/belinda.png",
+    name: "Belinda Walsh",
+    content:
+      "Using MindMap made journaling feel natural for me. The prompts help me reflect without overthinking, and I’ve become more aware of my emotions.",
+  },
+  {
+    id: 12,
+    src: "/images/belinda.png",
+    name: "Billy Hillary",
+    content: "The daily reflections and reminders keep me grounded.",
+  },
+  {
+    id: 13,
+    src: "/images/billy.png",
+    name: "Lucas Timothy",
+    content:
+      "The daily reflections and reminders keep me grounded. It doesn’t feel pushy — just supportive when I need it. Always Recommend",
+  },
+  {
+    id: 14,
+    src: "/images/belinda.png",
+    name: "Belinda Walsh",
+    content:
+      "Using MindMap made journaling feel natural for me. The prompts help me reflect without overthinking, and I’ve become more aware of my emotions.",
+  },
+  {
+    id: 15,
+    src: "/images/billy.png",
+    name: "Lucas Timothy",
+    content:
+      "The daily reflections and reminders keep me grounded. It doesn’t feel pushy — just supportive when I need it. Always Recommend",
+  },
+  {
+    id: 16,
     src: "/images/belinda.png",
     name: "Belinda Walsh",
     content:
